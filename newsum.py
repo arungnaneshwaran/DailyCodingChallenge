b119ac8eec7cbd77a8b2067869e153a9336ef35d
Arun kumar <arungnaneshwaran@gmail.com>
2018-08-24 15:44:54 +0530

    Update newsum.py

--- newsum.py
+++ newsum.py
@@ -1,19 +1,19 @@
 def fun(s,k):
-	s=set(s)
-	s=list(s)
-	if len(s)>=2 and sum(s)>=k:
-		i=0
-		j=len(s)-1
-		while i!=j:
-			if s[i]+s[j]==k:
-				return True
-			elif s[i]+s[j]>k:
-					j-=1
-			else:
-				i+=1
-	return False
-
-s=int(list(input("Enter the list: ")))
+    s=sorted(s)
+    print(s)
+    if len(s)>=2 and sum(s)>=k:
+        i=0
+        j=len(s)-1
+        while i!=j:
+            if s[i]+s[j]==k:
+                return True
+            elif s[i]+s[j]>k:
+                    j-=1
+            else:
+                i+=1
+    return False
+s = input('Enter your list: ')
+s = [int(x) for x in s.split(',')]
 k=int(input("Enter The sum value: "))
 a=fun(s,k)
 print(a)
